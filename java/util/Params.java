984b440edf1d98a2e4509924ab7a42702a84815c
hlpp <1013644379@qq.com>
2017-06-07 17:42:51 +0800

    .

---
+++ java/util/Params.java
@@ -0,0 +1,16 @@
+﻿package util;
+
+import java.util.HashMap;
+
+/**
+ * 参数DSL
+ * @author hulang
+ * @sice 2017年5月5日
+ * @vesion 1.0
+ */
+public class Params extends HashMap {
+    public Params add(String key, Object value) {
+        this.put(key, value);
+        return this;
+    }
+}
